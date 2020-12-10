01f4e6143436ea20c7329518cd271bba1e71a042
BeanpoleGames <31711256+BeanpoleGames@users.noreply.github.com>
2020-12-09 21:26:29 -0500

    Add pipeline file
    Agent not working, what image to use

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,1 +1,41 @@
+pipeline {
+    agent {
+    	docker {
+    		image 'jenkins'
+            args '--user root'
+    	}
+    }
 
+    stages {
+        stage('Setup') {
+            steps {
+                sh 'cat /etc/os-release'
+                sh 'rm -rf markturn-BeanpoleGames'
+                sh 'git clone https://github.com/UAComputerScience/markturn-BeanpoleGames.git'
+                sh 'cd markturn-BeanpoleGames'
+            }
+           
+        }
+        stage('CMake'){
+            steps{
+                sh 'mkdir -p finalbuild'
+                sh 'cd finalbuild; pwd; cmake ../markturn-BeanpoleGames -G Ninja'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'cd build; ninja'
+            }
+        }
+        stage('Test') {
+            steps {
+                ctest installation: 'cmake', workingDir: 'finalbuild'
+            }
+        }
+        stage('Package') {
+            steps {
+                cpack arguments: '-G DEB', installation: 'cmake', workingDir: 'finalbuild'
+            }
+        }
+    }
+}
