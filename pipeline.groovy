263a9393c8b6ef6cf30414718015c68aa4fada5f
BeanpoleGames <31711256+BeanpoleGames@users.noreply.github.com>
2020-12-11 14:54:19 -0500

    Add files via upload

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,4 +1,8 @@
 pipeline {
+	// Add ansicolor to make console output more readable
+    options {
+        ansiColor('xterm')
+    }
     agent {
         docker {
             image 'jpp52'
@@ -9,33 +13,40 @@ pipeline {
     stages {
         stage('Setup') {
             steps {
+            	echo '\033[33mSet up and fetch repository from GitHub\033[0m'
                 git branch: 'main', credentialsId: 'GitHub', url: 'https://github.com/UAComputerScience/markturn-BeanpoleGames.git'
             }
         }
         stage('CMake') {
             steps {
+            	echo '\033[33mCreate build directory and run CMake\033[0m'
                 cmakeBuild buildDir: 'build', cmakeArgs: '-D LINK_STATIC=OFF', generator: 'Ninja', installation: 'cmake'
             }
         }
         stage('Build') {
             steps {
+            	echo '\033[33mRun ninja to build\033[0m'
                 sh 'cd build; ninja'
             }
         }
         stage('Test') {
             steps {
+            	echo '\033[33mRun ctest to test program\033[0m'
                 ctest installation: 'cmake', workingDir: 'build'
             }
         }
         stage('Package') {
             steps {
+            	echo '\033[33mCreate .deb package using cpack\033[0m'
                 cpack arguments: '-G DEB', installation: 'cmake', workingDir: 'build'
             }
         }
     }
 
     post {
-        always {
+    	// Archive the .deb package on success of stages
+        success {
+        	echo '\033[33mArchive .deb package\033[0m'
             archiveArtifacts artifacts: 'build/*.deb', onlyIfSuccessful: true
         }
     }
